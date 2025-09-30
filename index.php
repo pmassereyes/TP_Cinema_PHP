b188cecb1bbcbed9707a3e6cdb29f223773777d6
maspa5z <pablomassereyes935@gmail.com>
2025-09-30 10:36:25 +0200

    ajout du tableau

--- index.php
+++ index.php
@@ -8,6 +8,14 @@ echo "Le film Bac Nord : ".$bacNord->getIdFilm() . " - " . $bacNord->getNomFilm(
 
 //Modifier le nombre d’entrées du film et afficher ses informations
 $bacNord->setNbEntrees(657450);
-echo "Le film Bac Nord : ".$bacNord->getIdFilm() . " - " . $bacNord->getNomFilm(). " - ". $bacNord->getNbEntrees();
+echo "Le film Bac Nord : ".$bacNord->getIdFilm() . " - " . $bacNord->getNomFilm(). " - ". $bacNord->getNbEntrees() . "\n";
 
-//Instancier d’autres objets de type « Film »
+//Instancier d’autres objets de type « Film » dans un tableau et afficher les informations de chaque film
+$films = [
+    new Film(2, "La vie d'Ilan", 2003458),
+    new Film(3, "La vie de Roni", 1523984),
+    new Film(4, "La vie de Ruben", 1200000),
+];
+foreach ($films as $film) {
+    echo "Infos sur le film :==> ". "\n" . $film->getIdFilm() . " - " . $film->getNomFilm(). " - ". $film->getNbEntrees() . "\n";
+}
