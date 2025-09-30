17116423e04561ea2c6c72f853130a5affb9c48c
maspa5z <pablomassereyes935@gmail.com>
2025-09-30 10:21:48 +0200

    changer les nbEntrees

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
 include  "Film.php";
+
+//Instancier un objet de type « Film » et afficher ses informations
 $bacNord = new Film(1, "BAC Nord", 563921);
+echo "Le film Bac Nord : ".$bacNord->getIdFilm() . " - " . $bacNord->getNomFilm(). " - ". $bacNord->getNbEntrees() . "\n";
+
+//Modifier le nombre d’entrées du film et afficher ses informations
+$bacNord->setNbEntrees(657450);
 echo "Le film Bac Nord : ".$bacNord->getIdFilm() . " - " . $bacNord->getNomFilm(). " - ". $bacNord->getNbEntrees();
+
+//Instancier d’autres objets de type « Film »
