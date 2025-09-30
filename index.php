ae30bb4413f85bedbc34b708667f3bd2df6d1739
maspa5z <pablomassereyes935@gmail.com>
2025-09-30 09:56:13 +0200

    new onjet film

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 
-include Film.php;
+include  "Film.php";
 $bacNord = new Film(1, "BAC Nord", 563921);
+echo "Le film Bac Nord : ".$bacNord->getIdFilm() . " - " . $bacNord->getNomFilm(). " - ". $bacNord->getNbEntrees();
