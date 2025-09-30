88225613b8b92741ae640fdf7b08f71ed65e6cbd
maspa5z <pablomassereyes935@gmail.com>
2025-09-30 11:05:19 +0200

    correction tableau

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 include  "Film.php";
 
 //Instancier un objet de type « Film » et afficher ses informations
-$bacNord = new Film(1, "BAC Nord", 563921);
-echo "Le film Bac Nord : ".$bacNord->getIdFilm() . " - " . $bacNord->getNomFilm(). " - ". $bacNord->getNbEntrees() . "\n";
+$filmF1 = new Film(1, "BAC Nord", 563921);
+echo "Le film Bac Nord : ".$filmF1->getIdFilm() . " - " . $filmF1->getNomFilm(). " - ". $filmF1->getNbEntrees() . "\n";
 
 //Modifier le nombre d’entrées du film et afficher ses informations
-$bacNord->setNbEntrees(657450);
-echo "Le film Bac Nord : ".$bacNord->getIdFilm() . " - " . $bacNord->getNomFilm(). " - ". $bacNord->getNbEntrees() . "\n";
+$filmF1->setNbEntrees(657450);
+echo "Modification du nombre d'entrées ==> " . "\n " . $filmF1->getIdFilm() . " - " . $filmF1->getNomFilm(). " - ". $filmF1->getNbEntrees() . "\n";
 
 //Instancier d’autres objets de type « Film » dans un tableau et afficher les informations de chaque film
 $films = [
-    new Film(2, "La vie d'Ilan", 2003458),
-    new Film(3, "La vie de Roni", 1523984),
-    new Film(4, "La vie de Ruben", 1200000),
+    $filmF2 = new Film(2, "Boîte noire", 2003458),
+    $filmF3 = new Film(3, "The Batman", 1523984),
 ];
 foreach ($films as $film) {
     echo "Infos sur le film :==> ". "\n" . $film->getIdFilm() . " - " . $film->getNomFilm(). " - ". $film->getNbEntrees() . "\n";
 }
+
+//Nous souhaitons maintenant connaître le réalisateur pour chaque film.
